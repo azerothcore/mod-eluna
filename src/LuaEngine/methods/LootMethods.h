13fc52847e120424ce144f564a62781cff15e6e8
EldrtathConnector <contact@warlog.dev>
2025-10-11 11:19:13 +0000

    Reapply changes from #323

--- src/LuaEngine/methods/LootMethods.h
+++ src/LuaEngine/methods/LootMethods.h
@@ -49,12 +49,19 @@ namespace LuaLoot
         uint16 loot_mode = ALE::CHECKVAL<uint16>(L, 6);
         bool needs_quest = ALE::CHECKVAL<bool>(L, 7, false);
 
-        for (LootItem &lootitem : loot->items)
+        if (allow_stacking)
         {
-            if (lootitem.itemid == itemid && lootitem.count < 255)
+            auto& container = needs_quest ? loot->quest_items : loot->items;
+
+            for (LootItem& lootitem : container)
             {
-                lootitem.count += min_count;
-                return 0;
+                if (lootitem.itemid == itemid && lootitem.count < 255)
+                {
+                    uint32 add = std::max<uint32>(1u, min_count);
+                    uint32 newCount = std::min<uint32>(255u, lootitem.count + add);
+                    lootitem.count = static_cast<uint8>(newCount);
+                    return 0;
+                }
             }
         }
 
